e28dc11cb699b03945d4e2c1d9712fa930befed1
Fatimah1403 <fattyhassan14@gmail.com>
2023-09-28 18:26:19 +0000

    Task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A function that returns  a list of lists of
+integers representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """function that create the pascal triangle"""
+    if n <= 0:
+        return []
+    # Lets loop through n
+    triangle = []
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
+    

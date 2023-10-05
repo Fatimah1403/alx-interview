0fcee8023c3eb18d55a7abc08d392ebf5b0fbe94
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-05 14:00:31 +0000

    Task updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,13 +11,13 @@ def canUnlockAll(boxes):
     if not boxes:
         return False
     n = len(boxes)
-    seen_boxes = set[0]
+    seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
     while len(unseen_boxes) > 0:
         box_id = unseen_boxes.pop()
         if not box_id or box_id < 0 or box_id >= n:
             continue
         if box_id not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(box_id[box_id])
+            unseen_boxes = unseen_boxes.union(boxes[box_id])
             seen_boxes.add(box_id)
     return n == len(seen_boxes)

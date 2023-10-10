fa5a20f44dc3be5e1f406fe7d94db5d029239d5d
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-10 02:46:29 +0000

    Solution added

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,11 +20,9 @@ def minOperations(n):
     if n <= 1:
         return res
 
-    i = 2
-
-    while n > 1:
+    for i in range(2, n + 1):
         while n % i == 0:
             res += i
-            n //= i
+            n /= i
 
     return res

9139c1e4060dd11a922803b11013eb478ecfb473
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-31 15:29:33 +0000

    Task added

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,43 +19,49 @@ class NQueens:
     Format: see example
     You don’t have to print the solutions in a specific order
     """
-    def __init__(self, n):
-        """Initialization"""
+     def __init__(self, n):
+        """ Global Variables """
         self.n = n
         self.x = [0 for i in range(n + 1)]
-        self.result = []
+        self.res = []
 
     def place(self, k, i):
-        """check if k Quueen can be placed in i column (true)
-            or if dey are attacking queens in row or diagonal (False)
+        """ Checks if k Queen can be placed in i column (True)
+        or if the are attacking queens in row or diagonal (False)
         """
+
+        # j checks from 1 to k - 1 (Up to previous queen)
         for j in range(1, k):
+            # There is already a queen in column
+            # or a queen in same diagonal
             if self.x[j] == i or \
                abs(self.x[j] - i) == abs(j - k):
                 return 0
         return 1
 
     def nQueen(self, k):
-        """Tries to place every queen in the board
-
+        """ Tries to place every queen in the board
         Args:
-         k: starting queen from which to evaluate (should be 1)
+        k: starting queen from which to evaluate (should be 1)
         """
+        # i goes from column 1 to column n (1st column is 1st index)
         for i in range(1, self.n + 1):
             if self.place(k, i):
                 # Queen can be placed in i column
                 self.x[k] = i
                 if k == self.n:
+                    # Placed all 4 Queens (A solution was found)
                     solution = []
                     for i in range(1, self.n + 1):
                         solution.append([i - 1, self.x[i] - 1])
-                    self.result.append(solution)
+                    self.res.append(solution)
                 else:
-                    # place morequeens
+                    # Need to place more Queens
                     self.nQueen(k + 1)
+        return self.res
 
-        return self.result
 
+# Main
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
@@ -66,15 +72,15 @@ N = sys.argv[1]
 try:
     N = int(N)
 except ValueError:
-    print("N must be a Number")
+    print("N must be a number")
     sys.exit(1)
 
 if N < 4:
     print("N must be at least 4")
     sys.exit(1)
 
-queen = NQueens(N)
-result = queen.nQueen(1)
+queen = NQueen(N)
+res = queen.nQueen(1)
 
-for i in result:
+for i in res:
     print(i)

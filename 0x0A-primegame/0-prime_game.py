f36856d2b5885b092ce884644d1894f57d86a8b3
Fatimah1403 <fattyhassan@gmail.com>
2023-12-07 19:20:14 -0500

    Task updated

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+0-prime_game
+"""
+
+
+def check_prime_nos(num):
+    """
+    Check for prime number
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    """
+    where x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+        if Maria > Ben:
+            return 'Maria'
+        else Ben > Maria:
+            return 'Ben'
+        return None
